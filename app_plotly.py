1085e1008c09d3137f35b810a6b2f9944f53c1c9
mesut-unal <m.unal1901@gmail.com>
2023-01-12 23:13:58 -0600

    discarding 0 cn nodes, chromosome num in color bar, fixing axes

--- app_plotly.py
+++ app_plotly.py
@@ -7,26 +7,27 @@ import pandas as pd
 import streamlit as st
 import pickle
 import plotly.graph_objects as go
+import plotly.express as px
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 # Cache the dataframe so it's only loaded once
 @st.experimental_memo
 def load_data():
-    lg = pickle.load(open('tmp/graph.txt','rb'))
+    longest_graph = pickle.load(open('tmp/graph.txt','rb'))
     df = pickle.load(open('tmp/dataframe.txt','rb')) # big dataframe
     df = df.sort_values(by=['Source'])
-    return lg,df
+    return longest_graph,df
 
 
 def main():
 
-    lg,data = load_data()
+    longest_graph,data = load_data()
 
     edge_x = []
     edge_y = []
-    for edge in lg.edges():
-        x0, y0 = lg.nodes[edge[0]]['pos']
-        x1, y1 = lg.nodes[edge[1]]['pos']
+    for edge in longest_graph.edges():
+        x0, y0 = longest_graph.nodes[edge[0]]['pos']
+        x1, y1 = longest_graph.nodes[edge[1]]['pos']
         edge_x.append(x0)
         edge_x.append(x1)
         edge_x.append(None)
@@ -43,39 +44,52 @@ def main():
     node_x = [] # position
     node_y = [] # copy number
     node_id = [] # node id
-    for node in lg.nodes():
-        x, y = lg.nodes[node]['pos']
+    node_chr = []
+    for node in longest_graph.nodes():
+        x, y = longest_graph.nodes[node]['pos']
         node_x.append(x)
         node_y.append(y)
         node_id.append(node)
 
-    # print(node_x)
-    # print(node_y)
-    # print(node_id)
+        node_chr.append(int(data[data['Source']==node].Chromosome.item()))
+
+    ### Compile hover text for each node
+    node_text = []
+    for n,node in enumerate(longest_graph.nodes()):
+        if n==len(longest_graph.nodes())-1:
+            next_node='None'
+        else:
+            next_node=list(longest_graph.nodes())[n+1]
+        node_text.append(f'Node:{node} - Next node:{next_node} - Chromosome:{node_chr[n]})')
+
     node_trace = go.Scatter(
         x=node_x, y=node_y,
         mode='markers',
         hoverinfo='text',
+        text=node_text,
         marker=dict(
             showscale=True,
             # colorscale options
             #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
             #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
             #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-            colorscale='YlGnBu',
+            # colorscale=px.colors.qualitative.Light24,
+            colorscale='Viridis',
             reversescale=True,
-            color=y,
+            color=node_chr,
             size=10,
+            symbol=0,
             colorbar=dict(
                 thickness=15,
-                title='Copy Number',
+                title='Chromosome Number',
                 xanchor='left',
                 titleside='right'
             ),
             line_width=2))
 
-    node_trace.text = node_id
-    node_trace.marker.color = node_y
+    # Can also be assigned later like below
+    # node_trace.text = node_id
+    # node_trace.marker.color = node_chr
 
 
     fig = go.Figure(data=[edge_trace, node_trace],
@@ -90,22 +104,20 @@ def main():
                     showarrow=False,
                     xref="paper", yref="paper",
                     x=0.1, y=1 ) ],
-                xaxis=dict(showgrid=False, zeroline=True, showticklabels=True),
-                yaxis=dict(showgrid=False, zeroline=True, showticklabels=True))
+                xaxis=dict(showgrid=True, zeroline=True, showticklabels=True,showline=True,title_text = "Copy Number"),
+                yaxis=dict(showgrid=True, zeroline=True, showticklabels=True,showline=True,title_text = "Start Point"))
                 )
-    #fig.show()
 
     st.plotly_chart(fig, use_container_width=True)
 
     with st.sidebar:
-
-
-
         # Style and Printing
         st.subheader('SV Graph Network Connections')
         st.write('M.Unal, MD Anderson Cancer Center')
         st.write('January 2023')
-        st.write('This plot shows the longest stractural variants chain found. Networkx is used for the network.')
+        st.write('This plot shows the longest stractural variants chain found from a JaBba output file. Networkx is used for the network.')
+        st.write('Hover through the nodes with your mouse to see the details of each node. Full screen and zooming is also possible on the graph. Moreover, the entire dataset is summarized in the table below.')
+
 
         st.subheader("All Nodes")
 
